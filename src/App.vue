1bcfb0cdf4c4cc556f03f64f761c98bd1cec7a13
Allan <a.maubert.jouvet@gmail.com>
2020-05-07 10:48:15 +0200

    add Buefy : Ui Library Component using Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="has-background-primary">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
@@ -17,13 +17,17 @@ import HelloWorld from './components/HelloWorld.vue';
 export default class App extends Vue {}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
+  @import "style";
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
 
 Vue.config.productionTip = false
+
+
+Vue.use(Buefy, {
+  defaultIconComponent: 'font-awesome-icon',
+  defaultIconPack: 'fas'
+})
 
 new Vue({
   render: h => h(App),
